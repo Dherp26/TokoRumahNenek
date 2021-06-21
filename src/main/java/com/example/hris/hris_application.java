ba28ee7d665dd2ff159cca8d4d9c7ce3351c290a
Dherp26 <spputra2699@gmail.com>
2021-06-21 12:05:02 +0700

    test 1

--- src/main/java/com/example/hris/hris_application.java
+++ src/main/java/com/example/hris/hris_application.java
@@ -2,9 +2,10 @@ package com.example.hris;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class hris_application {
+public class hris_application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(hris_application.class, args);
