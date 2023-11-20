bc13e78c5e75346aa81e331e294d4eca40d0c56d
Dherp26 <61307952+Dherp26@users.noreply.github.com>
2023-11-20 14:51:37 +0700

    Update data_overtime.java

--- src/main/java/com/example/hris/data_overtime/data_overtime.java
+++ src/main/java/com/example/hris/data_overtime/data_overtime.java
@@ -12,7 +12,6 @@ import java.util.UUID;
 @Entity
 public class data_overtime {
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private String id_overtime;
     private String nip;
     private String bulan;
